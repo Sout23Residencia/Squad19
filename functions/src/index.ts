04a80f219cf7deee6f95817bb67b2a34460ac9f3
Saitozin <afre.luz10@gmail.com>
2023-10-08 01:51:55 -0300

    teste de exportação de docx

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,16 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
+import * as fs from 'fs';
 import { db } from './config/firebase';
 import { doc, runTransaction } from 'firebase/firestore';
+import { Documento } from './documents/documentos';
+import { Packer } from 'docx';
+
 
 const app = express();
 
 app.get("/:id", async(req,res) => {
   const newDocRef = doc(db, "Pacientes", req.params.id);
-  let data
   try {
       await runTransaction(db, async (transaction) => {
         const sfDoc = await transaction.get(newDocRef);
@@ -15,9 +18,29 @@ app.get("/:id", async(req,res) => {
             return res.status(404).send("Documento Não Encontrado")          
           }
         else{
-            data = sfDoc.data()
-            return res.status(200).send({data : data});
-          }
+          const data = sfDoc.data();
+          const doc = Documento(data);
+          const buffer = await Packer.toBuffer(doc);
+          const fileName = "Teste.docx";
+          
+          fs.writeFile(fileName, buffer, (err) => {
+            if (err) {
+              return res.status(500).send("Erro ao salvar o arquivo");
+            }
+  
+            res.setHeader("Content-Disposition", "attachment; filename=Teste.docx");
+            res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            res.download(fileName, (err) => {
+              if (err) {
+                return res.status(500).send("Erro ao fazer o download do arquivo");
+              }
+              fs.unlinkSync(fileName); 
+              return res.status(200).send("Download Efetuado");
+            });
+            return res.status(200).send("Tudo Certo");
+          });
+          return res.status(200).send("Tudo Certo");
+        }
       });
     } catch (error) {
       return res.status(500).send("Mensagem de Erro: " + error)
