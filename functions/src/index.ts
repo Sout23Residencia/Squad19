d1231e29d7ccc1239d405f9038b7c42b9381db3d
Kayo Leanndro <kayoleanndro2@gmail.com>
2023-10-23 11:57:09 -0300

    [005] - KayoLeanndro - Atualizando estrutura

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,29 +1,51 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
+import * as fs from 'fs';
 import { db } from './config/firebase';
 import { doc, runTransaction } from 'firebase/firestore';
+import { Documento } from './documents/doc';
+import { Packer } from 'docx';
+
 
 const app = express();
 
 app.get("/:id", async(req,res) => {
-  const newDocRef = doc(db, "Pacientes", req.params.id);
+  const newDocRef = doc(db, "students", req.params.id);
   try {
       await runTransaction(db, async (transaction) => {
         const sfDoc = await transaction.get(newDocRef);
         if (!sfDoc.exists()) {
-          throw "Documento não existe!";
-          
+            return res.status(404).send("Documento Não Encontrado")          
           }
         else{
-          return res.send("aaaa" + {data : sfDoc.data});
-          }
+          const data = sfDoc.data();
+          const doc = Documento(data);
+          const buffer = await Packer.toBuffer(doc);
+          const fileName = "Teste.docx";
+          
+          fs.writeFile(fileName, buffer, (err) => {
+            if (err) {
+              return res.status(500).send("Erro ao salvar o arquivo");
+            }
+  
+            res.setHeader("Content-Disposition", "attachment; filename=Teste.docx");
+            res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            res.download(fileName, (err) => {
+              if (err) {
+                return res.status(500).send("Erro ao fazer o download do arquivo");
+              }
+              fs.unlinkSync(fileName); 
+              return res.status(200).send("Download Efetuado");
+            });
+            return res.status(200).send("Tudo Certo");
+          });
+          return res.status(200).send("Tudo Certo");
+        }
       });
-      console.log("Transação Completa!");
-    } catch (e) {
-      console.log("Transação falhou: ", e);
+    } catch (error) {
+      return res.status(500).send("Mensagem de Erro: " + error)
     }
-
-  return res.send("finalizado o get com sucesso ")
+    return res.send("Finalizado o GET com error indefinido");
 });
 
 exports.app = functions.https.onRequest(app);
