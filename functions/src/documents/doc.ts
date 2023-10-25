4f3f704e7a2e65e25c7fe91b68215940b3b1a82c
vitorcomv <vitorhdias11@gmail.com>
2023-10-25 16:18:35 -0300

    [0001] - Saito - Configuração do codigo

---
+++ functions/src/documents/doc.ts
@@ -0,0 +1,30 @@
+import { Document, Paragraph, Table, TableRow, TableCell, Packer} from "docx";
+import * as fs from "fs"
+import { DocumentData } from "firebase-admin/firestore";
+
+export function Documento(objectDate : DocumentData){
+
+    const doc = new Document({
+        sections: [
+            {
+                properties: {},
+                children: [
+                    new Table({
+                        rows: [
+                            new TableRow({
+                                children: [
+                                    new TableCell({
+                                        children: [new Paragraph("Nome: " + objectDate["name"])],
+                                    }),
+                                ],
+                            }),
+                        ],
+                    }),
+                ],
+            },
+        ],
+    });
+    Packer.toBuffer(doc).then((buffer) => {
+         fs.writeFileSync("testdocument.docx", buffer);
+    });
+}
