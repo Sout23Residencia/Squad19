b558fe56ae2c6d8a0a20389d192df7b31ebbb0de
Kayo Leanndro <kayoleanndro2@gmail.com>
2023-10-25 22:50:07 -0300

    atualização

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import * as fs from 'fs';
 import { db } from './config/firebase';
 import { doc, runTransaction } from 'firebase/firestore';
 import { Documento } from './documents/doc';
@@ -10,42 +9,24 @@ import { Packer } from 'docx';
 const app = express();
 
 app.get("/:id", async(req,res) => {
-  const newDocRef = doc(db, "students", req.params.id);
-  try {
-      await runTransaction(db, async (transaction) => {
-        const sfDoc = await transaction.get(newDocRef);
-        if (!sfDoc.exists()) {
-            return res.status(404).send("Documento Não Encontrado")          
-          }
-        else{
-          const data = sfDoc.data();
-          const doc = Documento(data);
-          const buffer = await Packer.toBuffer(doc);
-          const fileName = "Teste.docx";
-          
-          fs.writeFile(fileName, buffer, (err) => {
-            if (err) {
-              return res.status(500).send("Erro ao salvar o arquivo");
+    const newDocRef = doc(db, "Pacientes", req.params.id);
+    try {
+        await runTransaction(db, async (transaction) => {
+          const sfDoc = await transaction.get(newDocRef);
+          if (!sfDoc.exists()) {
+              return res.status(404).send("Documento Não Encontrado")
             }
-  
-            res.setHeader("Content-Disposition", "attachment; filename=Teste.docx");
-            res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-            res.download(fileName, (err) => {
-              if (err) {
-                return res.status(500).send("Erro ao fazer o download do arquivo");
-              }
-              fs.unlinkSync(fileName); 
-              return res.status(200).send("Download Efetuado");
-            });
-            return res.status(200).send("Tudo Certo");
-          });
-          return res.status(200).send("Tudo Certo");
-        }
-      });
-    } catch (error) {
-      return res.status(500).send("Mensagem de Erro: " + error)
-    }
-    return res.send("Finalizado o GET com error indefinido");
-});
+          else{
+            const data = sfDoc.data();
+            const documen = Documento(data);
+            const blob = Packer.toBlob(documen)
+            return res.status(200).send(blob);
+          }
+        });
+      } catch (error) {
+        return res.status(500).send("Mensagem de Erro: " + error)
+      }
+      return res.send("Finalizado o GET com error indefinido");
+  });
 
 exports.app = functions.https.onRequest(app);
