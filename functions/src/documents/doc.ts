3b9def154f3635c5174a6feac7109066eb11f0d4
Saitozin <afre.luz10@gmail.com>
2023-11-06 08:15:12 -0300

    [0005] - Saito - Upgrade do documento

--- functions/src/documents/doc.ts
+++ functions/src/documents/doc.ts
@@ -1,60 +1,101 @@
-import { Document, Packer, Paragraph, Table, TableCell, TableRow, TextRun } from 'docx';
+import { Document, Packer, Paragraph, ImageRun, Header, Footer, AlignmentType, BorderStyle, Table, TableCell, TableRow } from 'docx';
+import * as fs from 'fs';
 
 export function createDocumento(objectDate: any) {
-    const programData = objectDate.program;
+    const programData = objectDate.programs;
+    //const attendanceData = objectDate.attendances;
+    const behaviorData = objectDate.behaviors;
 
     let children = [
-        new Paragraph({
-            children: [new TextRun("Identificação do Paciente")]
-        }),
-        new Table({
-            rows: [
-                new TableRow({
-                    children: [
-                        new TableCell({
-                            children: [new Paragraph("Nome: " + objectDate["name"])],
-                        }),
-                        new TableCell({
-                            children: [new Paragraph("Data de Nascimento: " + objectDate["birthDate"])],
-                        }),
-                        new TableCell({
-                            children: [new Paragraph("ID: " + objectDate["id"])]
-                        })
-                    ]
-                })
-            ]
-        }),
+        new Paragraph("Estudante: " + objectDate["name"]),
+        new Paragraph("Clinica: "),
+        new Paragraph("Emitido em: " + objectDate.metadata["createdAt"]),
+        new Paragraph("Periodo: "),
     ];
 
-    if (programData && programData.length > 0) {
-        programData.forEach((programItem: any) => {
-            const programSection = new Table({
-                rows: [
-                    new TableRow({
-                        children: [
-                            new TableCell({
-                                children: [new Paragraph("Nome do Programa: " + programItem.name)],
-                            }),
-                            new TableCell({
-                                children: [new Paragraph("Area: " + programItem.area)],
-                            }),
-                        ]
-                    })
-                ]
-            });
+    const imageBuffer = fs.readFileSync('../functions/src/documents/img/logo-bhave.png');
 
-            children.push(new Paragraph({
-                children: [new TextRun("Programas")]
-            }));
+    const image = new ImageRun({
+        data: imageBuffer,
+        transformation: {
+            width: 140,
+            height: 60,
+        },
+    });
 
-            children.push(programSection);
-        });
-    }
+    const paragraphWithImage = new Paragraph({
+        children: [image],
+        alignment: AlignmentType.CENTER,
+    });
+
+    const description = new Paragraph({
+        text: "Relatórios de dados coletados",
+        alignment: AlignmentType.CENTER,
+        spacing: {
+            before: 10,
+            after: 10,
+        },
+    });
+
+    const line = new Paragraph({
+        border: {
+            bottom: {
+                style: BorderStyle.SINGLE,
+                size: 1,
+                color: "000000",
+            },
+        },
+    });
+
+    const paragraphs = programData.map((program: any) => {
+        return new Paragraph("Programa: " + program["name"] + "  |  Área: " + program["area"]);
+    });
+
+    const newParagraph2 = new Paragraph("Folha: DAV de Cores" + "  |  Tipo: DTT");
+
+    const header = new Header({
+        children: [paragraphWithImage, description],
+    });
+
+    const footer = new Footer({
+        children: [
+            new Paragraph('®bHave Tecnologia Comportamental'),
+            new Paragraph('RUA DO APOLO, 235. BAIRRO DO RECIFE, RECIFE, PE. CEP 50.030.220, CAIXA POSTAL 0015'),
+            new Paragraph('CNPJ: 24.832.498/0001-17 ● 55 81 3224.7289 ● contato@bhave.life ●  www.bhave.life'),
+        ],
+    });
+
+    const tabela = new Table({
+        rows: [
+            new TableRow({
+                children: [
+                    new TableCell({ children: [new Paragraph("Data")] }),
+                    new TableCell({ children: [new Paragraph("Terapeuta")] }),
+                    new TableCell({ children: [new Paragraph("Fase")] }),
+                ],
+            }),
+            ...behaviorData.map((behavior: any) => {
+                return new TableRow({
+                    children: [
+                        new TableCell({ children: [new Paragraph(behavior.metadata["createdAt"])] }),
+                        new TableCell({ children: [new Paragraph(" " + behavior.metadata.createdBy["firstName"] + " " + behavior.metadata.createdBy["lastName"] + " ")]}),
+                        new TableCell({ children: [new Paragraph(behavior["name"])]}),
+                    ],
+                });
+            }),
+        ],
+    });
 
     const doc = new Document({
         sections: [{
             properties: {},
-            children: children
+            children: [...children, line, ...paragraphs, newParagraph2, line, tabela],
+            headers: {
+                default: header,
+            },
+            footers: {
+                default: footer,
+            },
         }]
     });
 
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,41 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import { db } from './config/firebase';
-import { doc, runTransaction } from 'firebase/firestore';
+import { doc, runTransaction, collection, getDocs } from 'firebase/firestore';
 import { createDocumento } from './documents/doc';
 
 const app = express();
 
 app.get("", async (req, res) => {
-    const newDocRef = doc(db, "students", "471i66MXM4WPeUY345Lo");
+    const studentId = "CdvnW2cyHBzZZojNZxgC";
+    const studentRef = doc(db, "students", studentId);
 
     try {
         await runTransaction(db, async (transaction) => {
-            const sfDoc = await transaction.get(newDocRef);
-            if (!sfDoc.exists()) {
+            const studentDoc = await transaction.get(studentRef);
+            if (!studentDoc.exists()) {
                 return res.status(404).send("Documento Não Encontrado");
             } else {
-                const objectDate = sfDoc.data();
+                const studentData = studentDoc.data();
+                const attendancesRef = collection(studentRef, "attendances");
+                const behaviorsRef = collection(studentRef, "behaviors");
+                const programsRef = collection(studentRef, "programs");
+
+                const attendancesSnapshot = await getDocs(attendancesRef);
+                const behaviorsSnapshot = await getDocs(behaviorsRef);
+                const programsSnapshot = await getDocs(programsRef);
+
+                const attendancesData = attendancesSnapshot.docs.map(doc => doc.data());
+                const behaviorsData = behaviorsSnapshot.docs.map(doc => doc.data());
+                const programsData = programsSnapshot.docs.map(doc => doc.data());
+
+                const objectDate = {
+                    ...studentData,
+                    attendances: attendancesData,
+                    behaviors: behaviorsData,
+                    programs: programsData
+                };
+
                 const buffer = await createDocumento(objectDate);
                 res.setHeader('Content-Disposition', 'attachment; filename=documento.docx');
                 res.type('application/vnd.openxmlformats-officedocument.wordprocessingml.document');
